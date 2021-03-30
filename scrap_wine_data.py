fe8cc7efa56cd7f464ef21d330d4f3bab1a7dd89
Gustavo Rosa <gth.rosa@uol.com.br>
2021-03-29 23:15:15 -0300

    Moving dict creation to inside the loop.

--- scrap_wine_data.py
+++ scrap_wine_data.py
@@ -42,12 +42,12 @@ if __name__ == '__main__':
 
     print(f'Number of matches: {n_matches}')
 
-    # Creates a dictionary to hold the data
-    data = {}
-    data['wines'] = []
-
     # Iterates through the amount of possible pages
     for i in range(start_page, int(n_matches / c.RECORDS_PER_PAGE) + 1):
+        # Creates a dictionary to hold the data
+        data = {}
+        data['wines'] = []
+
         # Adds the page to the payload
         payload['page'] = i
 
